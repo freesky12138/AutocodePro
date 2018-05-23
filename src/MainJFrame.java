e34296be75a931aa6e7226bed425ff3a028a532b
freesky12138 <1774549483@qq.com>
2018-05-23 20:05:11 +0800

    haha point

--- src/MainJFrame.java
+++ src/MainJFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,7 +13,9 @@ import java.awt.event.ActionListener;
 public class MainJFrame implements ActionListener {
     private JFrame jf = new JFrame("测试窗口");          // 创建窗口
     private JButton btn;
-
+    private JTextField sqlTypeEdit;
+    private JTextPane sqlTypeText;
+    private JTextPane sqlTypeText2;
     public MainJFrame() {
         initJFrame();
 
@@ -22,12 +25,25 @@ public class MainJFrame implements ActionListener {
 
         // 3. 创建一个基本组件（按钮），并添加到 面板容器 中
         btn = new JButton("测试按钮");
+        sqlTypeEdit = new JTextField("数据库");
+        sqlTypeText=new JTextPane();
+        sqlTypeText.setText("数据库");
+        sqlTypeText.setEditable(false);
+
         panel.add(btn);
+        panel.add(sqlTypeEdit);
+        panel.add(sqlTypeText);
         btn.addActionListener(this);
+        sqlTypeText2=new JTextPane();
+        sqlTypeText2.setText("123");
+        JPanel panel2 = new JPanel();
+        panel2.add(sqlTypeText2);
 
         // 4. 把 面板容器 作为窗口的内容面板 设置到 窗口
-        jf.setContentPane(panel);
-
+        jf.add(panel);
+        jf.add(panel2);
+        // 5. 显示窗口，前面创建的信息都在内存中，通过 jf.setVisible(true) 把内存中的窗口显示在屏幕上。
+        jf.setVisible(true);
     }
 
     /**
@@ -38,9 +54,7 @@ public class MainJFrame implements ActionListener {
         jf.setLocationRelativeTo(null);             // 把窗口位置设置到屏幕中心
         jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // 当点击窗口的关闭按钮时退出程序（没有这一句，程序不会退出）
 
-        // 5. 显示窗口，前面创建的信息都在内存中，通过 jf.setVisible(true) 把内存中的窗口显示在屏幕上。
-        jf.setVisible(true);
-
+        jf.setLayout(new FlowLayout());
     }
 
     @Override
